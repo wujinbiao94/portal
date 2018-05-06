e3581481ff8ab8719bfe0d876e9d5bcff9a0e4a6
wujinbiao <4768872612qq.com>
2018-05-06 22:05:06 +0800

    新增空类待完成

---
+++ portal/src/main/java/service/PeccancyService.java
@@ -0,0 +1,7 @@
+package service;
+
+/**
+ * Created by 你敬爱的彪哥 on 2018/5/6.
+ */
+public interface PeccancyService {
+}
---
+++ portal/src/main/java/utils/servlet/PeccancyInsertServlet.java
@@ -0,0 +1,7 @@
+package utils.servlet;
+
+/**
+ * Created by 你敬爱的彪哥 on 2018/5/6.
+ */
+public class PeccancyInsertServlet {
+}
